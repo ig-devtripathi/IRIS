3920a541460a207bb14645e8d8b597a43a85193f
Dev Tripathi <dev25552@gmail.com>
2026-04-25 02:47:53 +0530

    Cleanup: Removed unnecessary debug and test files created by agents

--- test.py
+++
@@ -1,4 +0,0 @@
-import urllib.request, json
-req = urllib.request.Request('http://localhost:8000/api/run', data=b'{"mode":"pure","source":"simulated","n_processes":10}', headers={'Content-Type': 'application/json'})
-res = urllib.request.urlopen(req)
-print(json.dumps(json.loads(res.read())['metrics'], indent=2))
